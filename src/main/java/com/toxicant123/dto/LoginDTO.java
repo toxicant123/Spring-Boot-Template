f63167224a77073c896d7eee5bfa40147d3569a4
toxicant123 <13655847550@163.com>
2024-06-27 23:19:07 +0800

    SpringBootTemplate 0.0.4

---
+++ src/main/java/com/toxicant123/dto/LoginDTO.java
@@ -0,0 +1,17 @@
+package com.toxicant123.dto;
+
+import lombok.Data;
+
+/**
+ * @author toxicant123
+ * @version 1.0
+ * @Description
+ * @create 2024-06-27 下午11:18
+ */
+@Data
+public class LoginDTO {
+
+    private String username;
+
+    private String password;
+}

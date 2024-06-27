af18b7d5c6f20b9acdd0b561290eb576f79c166b
toxicant123 <13655847550@163.com>
2024-06-27 23:54:56 +0800

    SpringBootTemplate 0.0.7

---
+++ src/main/java/com/toxicant123/controller/AnimalController.java
@@ -0,0 +1,56 @@
+package com.toxicant123.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author toxicant123
+ * @version 1.0
+ * @Description
+ * @create 2024-06-27 下午11:48
+ */
+@Slf4j
+@RestController
+@RequestMapping("/animal")
+public class AnimalController {
+
+    private Map<String, String> animalNameMap = new HashMap<>();
+
+    {
+        animalNameMap.put("Elephant", "大象");
+        animalNameMap.put("Lion", "狮子");
+        animalNameMap.put("Tiger", "老虎");
+        animalNameMap.put("Giraffe", "长颈鹿");
+        animalNameMap.put("Zebra", "斑马");
+        animalNameMap.put("Monkey", "猴子");
+        animalNameMap.put("Penguin", "企鹅");
+        animalNameMap.put("Dolphin", "海豚");
+        animalNameMap.put("Kangaroo", "袋鼠");
+        animalNameMap.put("Koala", "考拉");
+    }
+
+    @GetMapping("/count")
+    public Integer animalCount() {
+        return 100;
+    }
+
+    @GetMapping("/names")
+    public List<String> animalNames() {
+        return animalNameMap.keySet().stream().toList();
+    }
+
+    @GetMapping("/nameSet")
+    public Set<String> animalNameSet() {
+        return animalNameMap.keySet();
+    }
+
+    @PostMapping
+    public String translateAnimalName(@RequestBody String animalName) {
+        return null;
+    }
+}
---
+++ src/main/java/com/toxicant123/dto/AnimalDTO.java
@@ -0,0 +1,10 @@
+package com.toxicant123.dto;
+
+/**
+ * @author toxicant123
+ * @version 1.0
+ * @Description
+ * @create 2024-06-27 下午11:54
+ */
+public class AnimalDTO {
+}

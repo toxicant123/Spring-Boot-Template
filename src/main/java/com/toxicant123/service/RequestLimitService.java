e755a171757168456b5e0bc862b26d0bc320fe22
toxicant123 <13655847550@163.com>
2024-08-09 08:23:08 +0800

    SpringBootTemplate 0.0.58

---
+++ src/main/java/com/toxicant123/service/RequestLimitService.java
@@ -0,0 +1,10 @@
+package com.toxicant123.service;
+
+/**
+ * @author toxicant123
+ * @version 1.0
+ * @Description
+ * @create 2024-08-09 上午8:22
+ */
+public interface RequestLimitService {
+}
---
+++ src/main/java/com/toxicant123/service/impl/RequestLimitServiceImpl.java
@@ -0,0 +1,16 @@
+package com.toxicant123.service.impl;
+
+import com.toxicant123.service.RequestLimitService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author toxicant123
+ * @version 1.0
+ * @Description
+ * @create 2024-08-09 上午8:22
+ */
+@Slf4j
+@Service
+public class RequestLimitServiceImpl implements RequestLimitService {
+}

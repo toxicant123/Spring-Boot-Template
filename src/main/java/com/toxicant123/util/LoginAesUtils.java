0691c77208744fac6bb771b0406da6a25d6d31cd
toxicant123 <13655847550@163.com>
2024-08-09 08:20:11 +0800

    SpringBootTemplate 0.0.57

--- src/main/java/com/toxicant123/util/LoginAesUtils.java
+++ src/main/java/com/toxicant123/util/LoginAesUtils.java
@@ -39,12 +39,11 @@ public class LoginAesUtils {
     }
 
     public static String encrypt(String text) {
-        String result;
         try {
             Cipher cipher = Cipher.getInstance(ALGORITHM);
             cipher.init(Cipher.ENCRYPT_MODE, SECRET_KEY_SPEC);
             var encryptedBytes = cipher.doFinal(text.getBytes());
-            result = Base64.getEncoder().encodeToString(encryptedBytes);
+            return Base64.getEncoder().encodeToString(encryptedBytes);
         } catch (NoSuchAlgorithmException e) {
             throw new LoginException(ErrorCodeAndUserMessageEnum.B0411, "NoSuchAlgorithm: " + ALGORITHM);
         } catch (NoSuchPaddingException
@@ -53,16 +52,14 @@ public class LoginAesUtils {
                  | BadPaddingException e) {
             throw new LoginException(ErrorCodeAndUserMessageEnum.A0350, "LoginAesUtils.encrypt error");
         }
-        return result;
     }
 
     public static String decrypt(String ciphertext) {
-        String result;
         try {
             Cipher cipher = Cipher.getInstance(ALGORITHM);
             cipher.init(Cipher.DECRYPT_MODE, SECRET_KEY_SPEC);
             var decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(ciphertext));
-            result = new String(decryptedBytes, StandardCharsets.UTF_8);
+            return new String(decryptedBytes, StandardCharsets.UTF_8);
         } catch (NoSuchAlgorithmException e) {
             throw new LoginException(ErrorCodeAndUserMessageEnum.B0411, "NoSuchAlgorithm: " + ALGORITHM);
         } catch (NoSuchPaddingException
@@ -71,6 +68,5 @@ public class LoginAesUtils {
                  | BadPaddingException e) {
             throw new LoginException(ErrorCodeAndUserMessageEnum.A0350, "LoginAesUtils.decrypt error");
         }
-        return result;
     }
 }

e4afc62539a7c859e7d32d462cd99af55ec1f3c1
toxicant123 <13655847550@163.com>
2024-08-09 08:18:30 +0800

    SpringBootTemplate 0.0.56

--- src/main/java/com/toxicant123/util/LoginAesUtils.java
+++ src/main/java/com/toxicant123/util/LoginAesUtils.java
@@ -1,5 +1,7 @@
 package com.toxicant123.util;
 
+import com.toxicant123.enums.ErrorCodeAndUserMessageEnum;
+import com.toxicant123.exception.unchecked.LoginException;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.crypto.*;
@@ -22,51 +24,52 @@ public class LoginAesUtils {
 
     private static final int KEY_SIZE = 128;
 
-    private static SecretKeySpec SECRET_KEY_SPEC;
+    private static final SecretKeySpec SECRET_KEY_SPEC;
 
     static {
         try {
             var keyGenerator = KeyGenerator.getInstance(ALGORITHM);
-            keyGenerator.init(KEY_SIZE); // 可以是128, 192, 或 256位
+            keyGenerator.init(KEY_SIZE);
             var secretKey = keyGenerator.generateKey();
             var keyBytes = secretKey.getEncoded();
             SECRET_KEY_SPEC = new SecretKeySpec(keyBytes, ALGORITHM);
         } catch (NoSuchAlgorithmException e) {
-            log.error("NoSuchAlgorithmException - {}", ALGORITHM, e);
-            System.exit(255);
+            throw new LoginException(ErrorCodeAndUserMessageEnum.B0411, "NoSuchAlgorithm: " + ALGORITHM);
         }
     }
 
     public static String encrypt(String text) {
-        String result = null;
+        String result;
         try {
             Cipher cipher = Cipher.getInstance(ALGORITHM);
             cipher.init(Cipher.ENCRYPT_MODE, SECRET_KEY_SPEC);
             var encryptedBytes = cipher.doFinal(text.getBytes());
             result = Base64.getEncoder().encodeToString(encryptedBytes);
-        } catch (NoSuchAlgorithmException
-                 | NoSuchPaddingException
+        } catch (NoSuchAlgorithmException e) {
+            throw new LoginException(ErrorCodeAndUserMessageEnum.B0411, "NoSuchAlgorithm: " + ALGORITHM);
+        } catch (NoSuchPaddingException
                  | InvalidKeyException
                  | IllegalBlockSizeException
                  | BadPaddingException e) {
-            log.error("LoginAesUtils.encrypt error", e);
+            throw new LoginException(ErrorCodeAndUserMessageEnum.A0350, "LoginAesUtils.encrypt error");
         }
         return result;
     }
 
     public static String decrypt(String ciphertext) {
-        String result = null;
+        String result;
         try {
             Cipher cipher = Cipher.getInstance(ALGORITHM);
             cipher.init(Cipher.DECRYPT_MODE, SECRET_KEY_SPEC);
             var decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(ciphertext));
             result = new String(decryptedBytes, StandardCharsets.UTF_8);
-        } catch (NoSuchAlgorithmException
-                 | NoSuchPaddingException
+        } catch (NoSuchAlgorithmException e) {
+            throw new LoginException(ErrorCodeAndUserMessageEnum.B0411, "NoSuchAlgorithm: " + ALGORITHM);
+        } catch (NoSuchPaddingException
                  | InvalidKeyException
                  | IllegalBlockSizeException
                  | BadPaddingException e) {
-            log.error("LoginAesUtils.decrypt error", e);
+            throw new LoginException(ErrorCodeAndUserMessageEnum.A0350, "LoginAesUtils.decrypt error");
         }
         return result;
     }

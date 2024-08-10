41e54737d1ff47b213f5a8d03b5dc4e9927ed2bc
toxicant123 <13655847550@163.com>
2024-08-10 16:42:02 +0800

    SpringBootTemplate 0.0.65

--- src/main/java/com/toxicant123/config/WebConfig.java
+++ src/main/java/com/toxicant123/config/WebConfig.java
@@ -1,12 +1,15 @@
 package com.toxicant123.config;
 
+import com.toxicant123.controller.*;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author toxicant123
@@ -24,8 +27,25 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        var unnecessaryAuthUrl = List.of(
+                "/api/login/*",
+                "/api/hello");
+
         registry.addInterceptor(new AuthInterceptor())
                 .addPathPatterns("/**") // 设置拦截器应用的路径模式
-                .excludePathPatterns("/login/*"); // 排除不需要拦截的路径
+                .excludePathPatterns(unnecessaryAuthUrl); // 排除不需要拦截的路径
+    }
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.addPathPrefix("/api", clazz -> Set
+                .of(
+                        AnimalController.class,
+                        ErrorController.class,
+                        HelloController.class,
+                        LoginController.class,
+                        ObjectController.class,
+                        UserController.class)
+                .contains(clazz));
     }
 }

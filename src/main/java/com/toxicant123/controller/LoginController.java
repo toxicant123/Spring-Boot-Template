1a4762826050d4d0981b022001e19c3681b5c309
toxicant123 <13655847550@163.com>
2024-08-10 13:26:29 +0800

    SpringBootTemplate 0.0.64

--- src/main/java/com/toxicant123/controller/LoginController.java
+++ src/main/java/com/toxicant123/controller/LoginController.java
@@ -6,10 +6,7 @@ import com.toxicant123.param.LoginParam;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author toxicant123
@@ -24,6 +21,11 @@ public class LoginController {
 
     @Autowired
     private LoginService loginService;
+
+    @GetMapping("/verify")
+    public String verify() {
+        return "success";
+    }
 
     @PostMapping("/usernameAndPassword")
     public LoginVO login(@RequestBody @Validated LoginParam param) {

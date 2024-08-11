304a6c1ae1158668ce76e4f7ebd85266d4030d81
toxicant123 <13655847550@163.com>
2024-08-11 09:35:10 +0800

    SpringBootTemplate 0.0.71

--- src/main/java/com/toxicant123/SpringBootTemplateApplication.java
+++ src/main/java/com/toxicant123/SpringBootTemplateApplication.java
@@ -3,7 +3,6 @@ package com.toxicant123;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// @MapperScan("com.toxicant123.dao")
 @SpringBootApplication
 public class SpringBootTemplateApplication {
 

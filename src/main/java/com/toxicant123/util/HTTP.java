b000bdd8eb8f800c557214386eee1488a013a4b7
toxicant123 <13655847550@163.com>
2025-02-19 23:23:29 +0800

    SpringBootTemplate 0.0.81

--- src/main/java/com/toxicant123/util/HTTP.java
+++ src/main/java/com/toxicant123/util/HTTP.java
@@ -29,13 +29,19 @@ import java.util.stream.Collectors;
 @Slf4j
 public class HTTP {
 
-    private static final Duration timeout = Duration.ofSeconds(60);
+    private static final Duration timeout = Duration.ofSeconds(10);
 
     private static final HttpClient httpClient = HttpClient
             .newBuilder()
             .followRedirects(HttpClient.Redirect.NORMAL)
-            .connectTimeout(Duration.ofSeconds(10))
+            .connectTimeout(timeout)
             .build();
+
+    private static HttpRequest.Builder getHttpRequestBuilder() {
+        return HttpRequest
+                .newBuilder()
+                .timeout(timeout);
+    }
 
     public static <T> T get(String url, Class<T> clazz) {
         return get(url, null, clazz);
@@ -54,11 +60,11 @@ public class HTTP {
     }
 
     public static <T> T get(String url, Map<String, String> params, Map<String, String> headers, Class<T> clazz) {
-        return request(HttpRequest.newBuilder().GET(), url, params, headers, str -> JSON.parseObject(str, clazz));
+        return request(getHttpRequestBuilder().GET(), url, params, headers, str -> JSON.parseObject(str, clazz));
     }
 
     public static <T> T get(String url, Map<String, String> params, Map<String, String> headers, TypeReference<T> typeReference) {
-        return request(HttpRequest.newBuilder().GET(), url, params, headers, str -> JSON.parseObject(str, typeReference));
+        return request(getHttpRequestBuilder().GET(), url, params, headers, str -> JSON.parseObject(str, typeReference));
     }
 
     public static <T> T post(String url, Class<T> clazz) {
@@ -86,11 +92,11 @@ public class HTTP {
     }
 
     public static <T> T post(String url, Object body, Map<String, String> params, Map<String, String> headers, Class<T> clazz) {
-        return request(HttpRequest.newBuilder().POST(getRequestBody(body)), url, params, headers, str -> JSON.parseObject(str, clazz));
+        return request(getHttpRequestBuilder().POST(getRequestBody(body)), url, params, headers, str -> JSON.parseObject(str, clazz));
     }
 
     public static <T> T post(String url, Object body, Map<String, String> params, Map<String, String> headers, TypeReference<T> typeReference) {
-        return request(HttpRequest.newBuilder().POST(getRequestBody(body)), url, params, headers, str -> JSON.parseObject(str, typeReference));
+        return request(getHttpRequestBuilder().POST(getRequestBody(body)), url, params, headers, str -> JSON.parseObject(str, typeReference));
     }
 
     private static HttpRequest.BodyPublisher getRequestBody(Object body) {
@@ -100,7 +106,6 @@ public class HTTP {
     }
 
     private static <T> T request(HttpRequest.Builder builder, String url, Map<String, String> params, Map<String, String> headers, Function<String, T> function) {
-        builder.timeout(timeout);
 
         if (ObjectUtils.isNotEmpty(params)) {
             url += params

5ee171c5cdfc8f15f58c6a617cd4d4c913bb5244
toxicant123 <13655847550@163.com>
2025-08-18 02:02:41 +0800

    SpringBootTemplate 0.0.131

--- src/main/java/com/toxicant123/controller/HelloController.java
+++ src/main/java/com/toxicant123/controller/HelloController.java
@@ -7,11 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-
 /**
  * @author toxicant123
  * @version 1.0
@@ -32,27 +27,6 @@ public class HelloController {
     public String error1() {
         int i = 1 / 0;
         return "error1";
-    }
-
-    @GetMapping("/error2")
-    public String error2() {
-        String a = null;
-        a.substring(1);
-        return "error2";
-    }
-
-    @GetMapping("/error3")
-    public String error3() {
-        if (1 > Math.random()) {
-        }
-        return "error3";
-    }
-
-    @GetMapping("/error4")
-    public String error4() throws FileNotFoundException {
-        var file = new File("D:\\abcdefg.txt");
-        var bufferedReader = new BufferedReader(new FileReader(file));
-        return "error3";
     }
 
     @GetMapping("/object")
